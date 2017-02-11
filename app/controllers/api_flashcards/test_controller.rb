270119b22473f294f07466fa4be53f7c8c463286
frol <frol.mr@ya.ru>
2017-02-11 14:56:27 +0300

    test controller and views

---
+++ app/controllers/api_flashcards/test_controller.rb
@@ -0,0 +1,7 @@
+module ApiFlashcards
+  class TestController < ApplicationController
+    def test1; end
+
+    def test2; end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,4 @@
 ApiFlashcards::Engine.routes.draw do
+  get 'test1' => 'test#test1'
+  get 'test2' => 'test#test2'
 end
